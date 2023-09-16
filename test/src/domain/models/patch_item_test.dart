dc6bcd7016d7f215ca5d7fd53bd81c24a298bd3a
yeasin50 <yeasinsheikh50@gmail.com>
2023-09-17 03:15:58 +0600

    fixed_analyzer-const_constructor

--- test/src/domain/models/patch_item_test.dart
+++ test/src/domain/models/patch_item_test.dart
@@ -4,20 +4,20 @@ import 'package:patch_pal/src/domain/models/patch_item.dart';
 void main() {
   group('Value Equality', () {
     test('same value should be identical', () {
-      final patchItem1 = PatchItem(name: 'enable rain', value: true);
-      final patchItem2 = PatchItem(name: 'enable rain', value: true);
+      const patchItem1 = PatchItem(name: 'enable rain', value: true);
+      const patchItem2 = PatchItem(name: 'enable rain', value: true);
       expect(patchItem1, patchItem2);
     });
 
     test('different value should not be identical', () {
-      final patchItem1 = PatchItem(name: 'enable rain', value: true);
-      final patchItem2 = PatchItem(name: 'enable rain', value: false);
+      const patchItem1 = PatchItem(name: 'enable rain', value: true);
+      const patchItem2 = PatchItem(name: 'enable rain', value: false);
       expect(patchItem1, isNot(patchItem2));
     });
 
     test('different value should not be identical message', () {
-      final patchItem1 = PatchItem(name: 'enable rain', value: true);
-      final patchItem2 = PatchItem(name: 'enable rain', value: false, message: 'test');
+      const patchItem1 = PatchItem(name: 'enable rain', value: true);
+      const patchItem2 = PatchItem(name: 'enable rain', value: false, message: 'test');
       expect(patchItem1, isNot(patchItem2));
     });
   });
@@ -46,7 +46,7 @@ void main() {
         expect(patchItem.message, 'test');
       });
 
-      test('handle int type value', ()  {
+      test('handle int type value', () {
         final data = {
           "name": "enable rain",
           "value": 1,
@@ -57,7 +57,7 @@ void main() {
         expect(patchItem.value, 1);
       });
 
-      test('handle null value', ()  {
+      test('handle null value', () {
         final data = {
           "name": "enable rain",
           "value": null,
